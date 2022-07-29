0cfc5053ddf948b425585f0548ad1660d140a1b4
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-07-29 17:17:06 +0400

    Module3 task6 DONE

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
+
     public void roundNumber(float numberToBeRounded) {
+        int result;
+        if (numberToBeRounded - (int) numberToBeRounded >= 0.5f) {
+            result = (int) numberToBeRounded + 1;
+        } else result = (int) numberToBeRounded;
+        System.out.println(result);
     }
 }
+
